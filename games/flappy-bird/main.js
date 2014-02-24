bf3b9a86bed84a600c5e5dde7372a35d39b46638
Kangcor <Kangcor@gmail.com>
2014-02-24 23:05:04 +0100

    Flappy bird pipe generator

--- games/flappy-bird/main.js
+++ games/flappy-bird/main.js
@@ -12,6 +12,9 @@ game_state.main.prototype = {
 
     	// Load the bird sprite
     	this.game.load.image('bird', 'assets/bird.png'); 
+
+    	//Load the pipe sprite
+    	this.game.load.image('pipe', 'assets/pipe.png'); 
     },
 
     create: function() { 
@@ -21,9 +24,14 @@ game_state.main.prototype = {
 	    // Add gravity to the bird to make it fall
 	    this.bird.body.gravity.y = 1000;  
 
+	    this.pipes = game.add.group();  
+		this.pipes.createMultiple(20, 'pipe');  
+
 	    // Call the 'jump' function when the spacekey is hit
 	    var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-	    space_key.onDown.add(this.jump, this);       
+	    space_key.onDown.add(this.jump, this); 
+
+	    this.timer = this.game.time.events.loop(1500, this.add_row_of_pipes, this);       
     },
     
     update: function() {
@@ -40,9 +48,36 @@ game_state.main.prototype = {
 
 	// Restart the game
 	restart_game: function() {  
+		
+		this.game.time.events.remove(this.timer);  
+
 	    // Start the 'main' state, which restarts the game
 	    this.game.state.start('main');
 	},
+
+	add_one_pipe: function(x, y) {  
+	    // Get the first dead pipe of our group
+	    var pipe = this.pipes.getFirstDead();
+
+	    // Set the new position of the pipe
+	    pipe.reset(x, y);
+
+	    // Add velocity to the pipe to make it move left
+	    pipe.body.velocity.x = -200; 
+
+	    // Kill the pipe when it's no longer visible 
+	    pipe.outOfBoundsKill = true;
+	},
+
+	add_row_of_pipes: function() {  
+    var hole = Math.floor(Math.random()*5)+1;
+
+    for (var i = 0; i < 8; i++)
+        if (i != hole && i != hole +1) 
+            this.add_one_pipe(400, i*60+10);   
+},
+
+
 };
 
 // Add and start the 'main' state to start the game
