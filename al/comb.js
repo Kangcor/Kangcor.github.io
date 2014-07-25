eddb5f479c712fc08899e80ce1a6f148e77c8f90
ddefabregas <ddefabregas@uic.es>
2014-07-25 11:27:43 +0200

    js testing for loop

--- al/comb.js
+++ al/comb.js
@@ -1,6 +1,15 @@
 $( document ).ready(function() {
-	$('#combinations').html = '7';
-
 	document.getElementById("combinations").innerHTML = "8";
+
+	var oTable = $('#combTable');
+	
+	for(i=0;i<=6666;i++)
+	{
+		var sRow = '<tr>';
+		sRow += "<td id='cell_"+i+"'>"+i+"</td>";
+		sRow += '</tr>';
+		oTable.append(sRow);
+	}
+
 });
 
