ef287feb4b865d9c7db2eb0a2c9e4f183341824a
ddefabregas <ddefabregas@uic.es>
2014-07-25 11:45:19 +0200

    glyphicons example 3

--- al/comb.js
+++ al/comb.js
@@ -5,8 +5,10 @@ $( document ).ready(function() {
 	for(i=0;i<=6666;i++)
 	{
 		var sRow = '<tr>';
-		sRow += "<td id='cell_"+i+"'>"+i+"<td class='glyphicon glyphicon-ok'></span>"+"</td>";
-		sRow += "<td class='glyphicon glyphicon-remove'></span>";
+		sRow += "<td id='cell_"+i+"'>"+i+"<span class='glyphicon glyphicon-ok'></span><span class='glyphicon glyphicon-remove'></span>"+"</td>";
+		sRow += "<td><span class='glyphicon glyphicon-remove'></span></td>";
+		sRow += "<td><span class='glyphicon glyphicon-ok'></span></td>";
+		sRow += "<td><span class='glyphicon glyphicon-remove'></span></td>";
 		sRow += '</tr>';
 		oTable.append(sRow);
 	}
